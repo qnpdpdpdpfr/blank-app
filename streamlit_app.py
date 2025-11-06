26622c749fc442309934cd56f961bec9cd1757cd
qnpdpdpdpfr <amollohaji@gmail.com>
2025-11-06 14:05:22 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
-pip install matplotlib
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # -----------------------------
 # 기본 설정
